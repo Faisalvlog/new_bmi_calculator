b1d286c7919aeb3e1f1c9dd4d47b76c738cdcdea
Faisalvlog <mfaisal.mfaisal496@gmail.com>
2020-11-25 18:08:59 +0500

    Make 2 refactor classes and edit with classes according to lecture 59 part 4

---
+++ lib/RepeatTextIconWidget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+class RepeatTextIconWidget extends StatelessWidget {
+  RepeatTextIconWidget({@required this.iconData,this.label});
+  final IconData iconData;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconData,
+          size: 90.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+              fontSize: 30.0,
+              color: Color(0xFF8D8E98)
+          ),),
+      ],
+    );
+  }
+}
