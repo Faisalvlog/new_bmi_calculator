d428e7aa555e369fcec5d1eba193b1252019045f
Faisalvlog <mfaisal.mfaisal496@gmail.com>
2020-11-25 18:03:18 +0500

    Refactor again in columns

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,4 +1,4 @@
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -24,9 +24,19 @@ class _MyHomePageState extends State<MyHomePage> {
               Expanded(
                   child:RepeatContainerCode(
                     colors: Color(0xFF1D1E33),
+                    cardwidget: RepeatTextIconWidget(
+                      iconData: FontAwesomeIcons.male,
+                      label: 'Male',
+                    ),
                   ),
               ),
-              Expanded(child:RepeatContainerCode(colors: Color(0xFF1D1E33),),
+              Expanded(child:RepeatContainerCode(
+                colors: Color(0xFF1D1E33),
+                cardwidget: RepeatTextIconWidget(
+                  iconData: FontAwesomeIcons.female,
+                  label: 'Female',
+                ),
+              ),
               ),
             ],
           ),),
@@ -46,14 +56,45 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class RepeatTextIconWidget extends StatelessWidget {
+  RepeatTextIconWidget({@required this.iconData,this.label});
+final IconData iconData;
+final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconData,
+          size: 90.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+          fontSize: 30.0,
+          color: Color(0xFF8D8E98)
+        ),),
+      ],
+    );
+  }
+}
+
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
+  RepeatContainerCode({@required this.colors, this.cardwidget});
   final Color colors;
+  final Widget cardwidget;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardwidget,
+
       decoration: BoxDecoration(
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
